219f22767926ac98649d33c2768baa42c9d4c501
John Oba <obajohn75@gmail.com>
2023-03-19 00:23:55 +0100

    chore(ui): added socail links

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="container py-5 sm:py-6 px-3 mx-auto max-w-5xl  border-t border-red">
-            <div class="flex justify-between py-4">
-        <div class="text-dark dark:text-gray-200">
-           <Icon name="ic:outline-copyright"  /> 2023 AfroDev
-        </div>
-        <div class="socials">
-            <Icon name="logos:twitter" class="mx-2  cursor-pointer" />
-            <Icon name="skill-icons:linkedin" class="mx-2  cursor-pointer" />
-            <Icon name="skill-icons:instagram" class="mx-2  cursor-pointer" />
-            <Icon name="skill-icons:github-light" class="mx-2  cursor-pointer" />
-        </div>
-    </div>
-    </div>
+        <div class="flex justify-between py-4">
+            <div class="text-dark dark:text-gray-200">
+                <Icon name="ic:outline-copyright" /> 2023 AfroDev
+            </div>
+            <div class="socials">
+                <a href="https://twitter.com/_afrodev" target="_blank">
+                    <Icon name="logos:twitter" class="mx-2  cursor-pointer" />
+                </a>
+                <a href="https://linkedin.com/in/johnoba" target="_blank">
+                    <Icon name="skill-icons:linkedin" class="mx-2  cursor-pointer" />
+                </a>
+                <a href="https://github.com/johnexzy" target="_blank">
+                    <Icon name="skill-icons:github-light" class="mx-2  cursor-pointer" />
+                </a>
 
-</template>
+        </div>
+    </div>
+</div></template>
