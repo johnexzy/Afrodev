2b1a682ad3b5b873847934bf7a742e7c78741b74
Johnexzy <obajohn75@gmail.com>
2023-03-22 08:44:49 +0100

    ui: fixed no-wrap on toggle image

--- pages/index.vue
+++ pages/index.vue
@@ -21,12 +21,12 @@
             <div
               class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
             </div>
-            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Toggle Images</span>
+            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Images</span>
           </label>
         </div>
       </div>
 
-      <ListArticles :data="data" :hide-images="hideImages" />
+      <ListArticles :data="data" :hide-images="!hideImages" />
     </div>
     <div class="my-10 h- flex items-center justify-center">
       <div>
@@ -71,7 +71,7 @@
 </template>
 <script setup lang="ts">
 import { withBase } from 'ufo'
-const hideImages = ref(true)
+const hideImages = ref(false)
 
 
 const data = await queryContent('/')
